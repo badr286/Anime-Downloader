4c8f3358c92baaf0f4e66d4dacdbd9a12fbd9906
badr286 <52196096+badr286@users.noreply.github.com>
2022-06-26 22:07:51 +0300

    Adding Yonaplay

--- anime_classes.py
+++ anime_classes.py
@@ -1,6 +1,13 @@
 from requests import get
 from bs4 import BeautifulSoup as soup
 
+
+def merge_lists(From,To):
+    for i in From:
+        To.append(i)
+    return True
+
+
 class Witanime:
 
     def get_servers_from_multi_servers(multi_servers_url):
@@ -8,7 +15,7 @@ class Witanime:
         res = get(url)
         soup_res = soup(res.text, 'html.parser')
 
-        servers_LIs = [li for li in soup_res.findAll('li')]
+        servers_LIs = soup_res.findAll('li')
         servers = []
 
         for server_LI in servers_LIs:
@@ -21,20 +28,42 @@ class Witanime:
 
         return servers
 
+    def get_servers_from_yonaplay(yonaplayer_url):
+        res = get(yonaplayer_url, headers = {'referer':'https://witanime.com'})
+        soup_res = soup( res.text, 'html.parser' )
+
+        container_div = soup_res.find('div', {'class': 'ODDIV'})
+        servers_LIs = container_div.findAll('li')
+
+        servers = []
+        for server_LI in servers_LIs:
+            name = server_LI.find('span').text + ' ' + server_LI.find('p').text
+            url = server_LI['onclick'].replace('go_to_player', '').replace('(', '').replace(')', '').replace("'", '')
+            
+            servers.append({'name': name, 'url': url})
+
+        return servers
+    
+
     def get_ep_sources_by_ep_url(ep_url):
         res = get(ep_url)
         soup_res = soup(res.text, 'html.parser')
 
         servers_a_tags = soup_res.find(id='episode-servers').findAll('a')
-
-        # IN CASE MULTISERVERS DOESN'T EXIST
         servers = []
         
-        # FINDING THE MULTI SERVER
+        # FINDING THE MULTI SERVERS
         for server in servers_a_tags:
             if 'multi server' in server.text:
                 url = server['data-ep-url']
-                servers = Witanime.get_servers_from_multi_servers(url)
+                multiservers = Witanime.get_servers_from_multi_servers(url)
+                merge_lists(multiservers, servers)
+                servers_a_tags.remove(server)
+                
+            elif 'yonaplay' in server.text:
+                url = server['data-ep-url']
+                yonaservers = Witanime.get_servers_from_yonaplay(url)
+                merge_lists(yonaservers, servers)
                 servers_a_tags.remove(server)
 
 

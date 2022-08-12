9d7d7480476271c4c1488f7af8e1b26ca0f0c9f4
badr286 <52196096+badr286@users.noreply.github.com>
2022-08-13 00:35:10 +0300

    Slight Update
    - Added New Servers
    - Added A Way To Avoid Crashing If A Non-Available Server(s) Is/Are Chosen

--- downloader.py
+++ downloader.py
@@ -1,11 +1,13 @@
-from downloading_classes import GoogleDrive, Mp4upload, Userscloud, Tusfiles, Bayfiles
+from downloading_classes import GoogleDrive, Mp4upload, Userscloud, Tusfiles, Bayfiles, Workupload, Mediafire
 
 classes_and_identifiers = {
     'drive': GoogleDrive,
     'mp4upload': Mp4upload,
     'userscloud': Userscloud,
     'tusfiles': Tusfiles,
-    'bayfiles': Bayfiles
+    'bayfiles': Bayfiles,
+    'workupload': Workupload,
+    'mediafire': Mediafire
     
 }
 
@@ -15,6 +17,7 @@ def get_file(url):
     for identifier in identifiers:
         if identifier in url:
             the_class = classes_and_identifiers[identifier]
-
             return the_class.get_file_by_url(url)
+    return False
+    
         
